7f2154d631e81da905ec835ba706b330f0db8819
gidl59 <giuseppedilisio16@gmail.com>
2025-11-19 15:07:53 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const callLinks = document.querySelectorAll(".js-call-link");
 
+  callLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      const number = this.dataset.number || this.getAttribute("href");
+      const msg = `Vuoi chiamare questo numero?\n\n${number}`;
+      const ok = window.confirm(msg);
+      if (!ok) {
+        e.preventDefault();
+      }
+    });
+  });
+});
